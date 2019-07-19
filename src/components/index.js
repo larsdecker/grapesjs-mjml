31684e70d92692710f51d75e6d12309f61ec35e0
Michelangelo Bagnara <michelangelo.developer@gmail.com>
2019-07-19 14:38:31 +0200

    Fixed mj-body attributes.

--- src/components/index.js
+++ src/components/index.js
@@ -41,8 +41,8 @@ export default (editor, opt = {}) => {
     // MJML Core model
     let coreMjmlModel = {
         init() {
-            const attrs = {...this.get('attributes')};
-            const style = {...this.get('style')};
+            const attrs = { ...this.get('attributes') };
+            const style = { ...this.get('style') };
 
             for (let prop in style) {
                 if (!(prop in attrs)) {
@@ -57,7 +57,7 @@ export default (editor, opt = {}) => {
 
 
         handleStyleChange() {
-            const style = {...this.get('attributes'), ...this.get('style')};
+            const style = { ...this.get('attributes'), ...this.get('style') };
             this.set('attributes', style);
         },
 
@@ -77,8 +77,8 @@ export default (editor, opt = {}) => {
          * @return {Object}
          */
         getAttrToHTML() {
-            const attr = {...this.get('attributes')};
-            const style = {...this.get('style-default')};
+            const attr = { ...this.get('attributes') };
+            const style = { ...this.get('style-default') };
             delete attr.style;
 
             for (let prop in attr) {
@@ -161,7 +161,7 @@ export default (editor, opt = {}) => {
             }
 
             return {
-                start: `<${tagName} ${strAttr}>`,
+                start: `<${tagName}${strAttr}>`,
                 end: `</${tagName}>`,
             };
         },
@@ -204,7 +204,7 @@ export default (editor, opt = {}) => {
                 });
                 this.childNodes = this.componentsView.render(container).el.childNodes;
             } else {
-                this.componentsView.parent = container;
+                this.componentsView.parentEl = container;
             }
 
             var childNodes = Array.prototype.slice.call(this.childNodes);
