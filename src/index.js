6c8af910d978d4f6ed447ea20f970109b9717f9f
Michelangelo Bagnara <michelangelo.developer@gmail.com>
2019-07-22 09:25:59 +0200

    Indentation.

--- src/index.js
+++ src/index.js
@@ -1,106 +1,107 @@
 export default (editor, opts = {}) => {
-    let c = opts || {};
-    let config = editor.getConfig();
+  let c = opts || {};
+  let config = editor.getConfig();
 
-    let defaults = {
-        editor,
-        cmdBtnMoveLabel: 'Move',
-        cmdBtnUndoLabel: 'Undo',
-        cmdBtnRedoLabel: 'Redo',
-        cmdBtnDesktopLabel: 'Desktop',
-        cmdBtnTabletLabel: 'Tablet',
-        cmdBtnMobileLabel: 'Mobile',
 
-        expTplBtnTitle: c.expTplBtnTitle || 'View Code',
-        fullScrBtnTitle: c.fullScrBtnTitle || 'FullScreen',
-        swichtVwBtnTitle: c.swichtVwBtnTitle || 'View Components',
-        defaultTemplate: '', // Default template in case the canvas is empty
-        categoryLabel: '',
+  let defaults = {
+    editor,
+    cmdBtnMoveLabel: 'Move',
+    cmdBtnUndoLabel: 'Undo',
+    cmdBtnRedoLabel: 'Redo',
+    cmdBtnDesktopLabel: 'Desktop',
+    cmdBtnTabletLabel: 'Tablet',
+    cmdBtnMobileLabel: 'Mobile',
 
-        // Code viewer theme
-        codeViewerTheme: 'hopscotch',
+    expTplBtnTitle: c.expTplBtnTitle || 'View Code',
+    fullScrBtnTitle: c.fullScrBtnTitle || 'FullScreen',
+    swichtVwBtnTitle: c.swichtVwBtnTitle || 'View Components',
+    defaultTemplate: '', // Default template in case the canvas is empty
+    categoryLabel: '',
 
-        // Import placeholder MJML
-        importPlaceholder: '',
+    // Code viewer theme
+    codeViewerTheme: 'hopscotch',
 
-        // Title for the import modal
-        modalTitleImport: 'Import MJML',
+    // Import placeholder MJML
+    importPlaceholder: '',
 
-        // Test for the import button
-        modalBtnImport: 'Import',
+    // Title for the import modal
+    modalTitleImport: 'Import MJML',
 
-        // Description for the import modal
-        modalLabelImport: '',
+    // Test for the import button
+    modalBtnImport: 'Import',
 
-        // Title for the export modal
-        modalTitleExport: 'Export MJML',
+    // Description for the import modal
+    modalLabelImport: '',
 
-        // Description for the export modal
-        modalLabelExport: '',
+    // Title for the export modal
+    modalTitleExport: 'Export MJML',
 
-        // Overwrite default export command
-        overwriteExport: 1,
+    // Description for the export modal
+    modalLabelExport: '',
 
-        // String before the MJML in export code
-        preMjml: '',
+    // Overwrite default export command
+    overwriteExport: 1,
 
-        // String after the MJML in export code
-        postMjml: '',
+    // String before the MJML in export code
+    preMjml: '',
 
-        // Export 'mjml', 'html' or both (leave empty) TODO
-        exportOnly: '',
+    // String after the MJML in export code
+    postMjml: '',
 
-        // Clean all previous blocks if true
-        resetBlocks: 1,
+    // Export 'mjml', 'html' or both (leave empty) TODO
+    exportOnly: '',
 
-        // Reset the Style Manager and add new properties for MJML
-        resetStyleManager: 1,
+    // Clean all previous blocks if true
+    resetBlocks: 1,
 
-        // Column padding (this way it's easier select columns)
-        columnsPadding: '10px 0',
+    // Reset the Style Manager and add new properties for MJML
+    resetStyleManager: 1,
 
-        ...opts,
-    };
+    // Column padding (this way it's easier select columns)
+    columnsPadding: '10px 0',
 
-    // Change some config
-    config.devicePreviewMode = 1;
+    ...opts,
+  };
 
-    // Load defaults
-    for (let name in defaults) {
-        if (!(name in c))
-            c[name] = defaults[name];
-    }
+  // Change some config
+  config.devicePreviewMode = 1;
 
-    // I need to prevent forced class creation as classes aren't working
-    // at the moment
-    config.forceClass = 0;
+  // Load defaults
+  for (let name in defaults) {
+    if (!(name in c))
+      c[name] = defaults[name];
+  }
 
-    // Don't need to create css rules with media
-    config.devicePreviewMode = 1;
+  // I need to prevent forced class creation as classes aren't working
+  // at the moment
+  config.forceClass = 0;
 
-    // Add Blocks
-    require('./blocks').default(editor, opts);
+  // Don't need to create css rules with media
+  config.devicePreviewMode = 1;
 
-    // Add Components
-    require('./components').default(editor, opts);
+  // Add Blocks
+  require('./blocks').default(editor, opts);
 
-    // Add Commands
-    require('./commands').default(editor, opts);
+  // Add Components
+  require('./components').default(editor, opts);
 
-    // Add Buttons
-    require('./buttons').default(editor, opts);
+  // Add Commands
+  require('./commands').default(editor, opts);
 
-    // Extend Style Manager
-    require('./style').default(editor, opts);
+  // Add Buttons
+  require('./buttons').default(editor, opts);
 
-    // Update devices
-    if (opts.resetDevices) {
-        const dm = editor.DeviceManager;
-        dm.getAll().reset();
-        dm.add('Desktop', '');
-        dm.add('Mobile', '320px');
-        dm.add('Tablet', '820px');
-    }
+  // Extend Style Manager
+  require('./style').default(editor, opts);
+
+  // Update devices
+  if (opts.resetDevices) {
+    const dm = editor.DeviceManager;
+    dm.getAll().reset();
+    dm.add('Desktop', '');
+    dm.add('Mobile', '320px');
+    dm.add('Tablet', '820px');
+  }
 
 
 };
